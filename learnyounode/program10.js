498f611136ee52ad1a2b0eb204fe43123d4c1e30
juandados <juandados@gmail.com>
2018-03-15 09:59:33 -0500

    solving problem 10

--- learnyounode/program10.js
+++ learnyounode/program10.js
@@ -1,8 +1,19 @@
 var port_number = Number(process.argv[2]);
 var net = require('net');
 var server = net.createServer( function listener(socket){
-  socket.write("hdda");
-  socket.pipe(socket);
-  //socket.end(String(port_number));
+  function zero_filler(original_str, final_length){
+    filled_string = "";
+    for (i = 1; i <= (final_length - original_str.length); i++) {
+      filled_string += "0";
+    }
+    return filled_string + original_str;
+  }
+  date = new Date()
+  YYYY = zero_filler(String(date.getFullYear()),4);
+  MM = zero_filler(String(date.getMonth() + 1),2); // starts at 0 
+  DD = zero_filler(String(date.getDate()),2);      // returns the day of month  
+  hh = zero_filler(String(date.getHours()),2);
+  mm = zero_filler(String(date.getMinutes()),2);
+  socket.end(`${YYYY}-${MM}-${DD} ${hh}:${mm}\n`);
 })
-server.listen(port_number,'127.0.0.1');
+server.listen(port_number);
